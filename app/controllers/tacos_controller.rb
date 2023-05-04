a6c442e0c3ff2306b8f6bdaf329578ca44ee3b2f
Fumiya Kai <fumiya.kai@kellogg.northwestern.edu>
2023-05-04 02:23:57 +0000

    commit

---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,7 @@
+class TacosController < ApplicationController
+  def index
+    # html = "<h1>Tacos</h1><p>Let's eat delish tacos!</p><ul></ul>"
+    # render :inline => html
+    render :template => "tacos/index"
+  end
+end
